06b4e2bade95f0c8350738a2171e22b9b6d8288d
sebastianmr18 <juan.munoz.rojas@correounivalle.edu.co>
2024-11-10 19:55:25 -0500

    feat: Initial configuration

---
+++ authentication/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RegisterUserView, RegisterWarehouseAssistantView, LoginView
+
+urlpatterns = [
+    path('register/user', RegisterUserView.as_view(), name='register_user'),
+    path('register/warehouse-assistant', RegisterWarehouseAssistantView.as_view(), name='register_warehouse_assistant'),
+    path('login', LoginView.as_view(), name='login'),
+]
